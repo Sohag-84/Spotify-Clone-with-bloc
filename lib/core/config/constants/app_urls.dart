5f3ce1f525206fecd3e2ae48ef63f00e57f60925
Sohag-84 <ihsohag84@gmail.com>
2024-07-17 09:57:05 +0600

    fetched song

--- lib/core/config/constants/app_urls.dart
+++ lib/core/config/constants/app_urls.dart
@@ -1,16 +1,27 @@
 class AppUrls {
-  static const String firestorage =
+  static const String coverFirestorage =
+      "https://firebasestorage.googleapis.com/v0/b/spotify-clone-d2bfd.appspot.com/o/covers%2F";
+  static const String songFirestorage =
       "https://firebasestorage.googleapis.com/v0/b/spotify-clone-d2bfd.appspot.com/o/covers%2F";
 
   static const String mediaAlt = "alt=media";
 }
 
+List<String> coverPhotoList = [
+  "${AppUrls.coverFirestorage}photo-1.webp?${AppUrls.mediaAlt}",
+  "${AppUrls.coverFirestorage}photo-2.webp?${AppUrls.mediaAlt}",
+  "${AppUrls.coverFirestorage}photo-3.webp?${AppUrls.mediaAlt}",
+  "${AppUrls.coverFirestorage}photo-4.webp?${AppUrls.mediaAlt}",
+  "${AppUrls.coverFirestorage}photo-5.webp?${AppUrls.mediaAlt}",
+  "${AppUrls.coverFirestorage}photo-6.webp?${AppUrls.mediaAlt}",
+];
 
-List<String> coverPhotoList =[
-  "${AppUrls.firestorage}photo-1.webp?${AppUrls.mediaAlt}",
-  "${AppUrls.firestorage}photo-2.webp?${AppUrls.mediaAlt}",
-  "${AppUrls.firestorage}photo-3.webp?${AppUrls.mediaAlt}",
-  "${AppUrls.firestorage}photo-4.webp?${AppUrls.mediaAlt}",
-  "${AppUrls.firestorage}photo-5.webp?${AppUrls.mediaAlt}",
-  "${AppUrls.firestorage}photo-6.webp?${AppUrls.mediaAlt}",
+List<String> songUrlList = [
+  //flow-211881
+  "${AppUrls.songFirestorage}flow-211881.mp3?${AppUrls.mediaAlt}",
+  "${AppUrls.songFirestorage}	for-her-chill-upbeat-summel-travel-vlog-and-ig-music-royalty-free-use-202298.mp3?${AppUrls.mediaAlt}",
+  "${AppUrls.songFirestorage}in-slow-motion-inspiring-ambient-lounge-219592.mp3?${AppUrls.mediaAlt}",
+  "${AppUrls.songFirestorage}inspiring-cinematic-ambient-116199.mp3?${AppUrls.mediaAlt}",
+  "${AppUrls.songFirestorage}lotus-sky-dreams-216049.mp3?${AppUrls.mediaAlt}",
+  "${AppUrls.songFirestorage}sweet-serenade-222444.mp3?${AppUrls.mediaAlt}",
 ];
